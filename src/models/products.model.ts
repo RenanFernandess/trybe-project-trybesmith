2d2be8fb2fbd63401b8fd94b19fa98dd386f5c26
Renan Fernandes <renzinestuods@gmail.com>
2022-12-14 16:42:58 -0300

    :sparkles: feat: Cria a função set que seta um produdo nom banco de dados e retorna o produto adicionado

---
+++ src/models/products.model.ts
@@ -0,0 +1,17 @@
+import { ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import TProduct from '../types/product';
+import connection from './database/connection';
+
+const set = async ({ name, amount, orderId }: TProduct): Promise<TProduct[]> => {
+  const INSERT_QUERY = 'INSERT INTO Trybesmith.products (name, amount, order_id) VALUES (?, ?, ?)';
+  const SELECT_QUERY = 'SELECT id, name, amount FROM Trybesmith.products WHERE id =?';
+  const [{ insertId }] = await connection
+    .execute<ResultSetHeader>(INSERT_QUERY, [name, amount, orderId]);
+  const [result] = await connection.execute<RowDataPacket[] & TProduct[]>(SELECT_QUERY, [insertId]);
+
+  return result;
+};
+
+export default {
+  set,
+};
