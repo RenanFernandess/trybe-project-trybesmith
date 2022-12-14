4e5cc978cf16d6f873dcdc39c434ed6df00d679a
Renan Fernandes <renzinestuods@gmail.com>
2022-12-14 17:08:13 -0300

    :sparkles: feat: Cria a função getAll, que retorna a resposta da requisição

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from 'express';
-import porductsService from '../services/products.service';
+import productsService from '../services/products.service';
+import TStatus from '../types/status';
 
 const set = async (req: Request, res: Response): Promise<void> => {
   const { body: { name, amount } } = req;
 
-  const { status, message } = await porductsService.set({ name, amount });
+  const { status, message }: TStatus = await productsService.set({ name, amount });
   if (status) { res.status(status).json({ message }); return; }
   
   res.status(201).json(message);
 };
 
+const getAll = async (_req: Request, res: Response) => {
+  const { status, message }: TStatus = await productsService.getAll();
+  if (status) { res.status(status).json({ message }); return; }
+
+  res.status(200).json(message);
+};
+
 export default {
   set,
+  getAll,
 };
