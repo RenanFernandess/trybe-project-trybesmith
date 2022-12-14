61e3d1bc2ee3473bb2f4c0c49f9415a437e93a04
Renan Fernandes <renzinestuods@gmail.com>
2022-12-14 17:09:01 -0300

    :sparkles: feat: Adiciona a rota get '/'

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/products.controller';
 const productsRoute = Router();
 
 productsRoute.post('/', productsController.set);
+productsRoute.get('/', productsController.getAll);
 
 export default productsRoute;
