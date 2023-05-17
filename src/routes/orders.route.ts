a33c0a237c1c2d6c4a064ada5187b59e9955d7a2
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 19:46:55 -0300

    :sparkles: feat: Cria a rota POST '/orders'

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import ordersController from '../controllers/orders.controller';
+import validateToken from '../middlewares/validateToken';
 
 const ordersRoute = Router();
 
 ordersRoute.get('/', ordersController.getAll);
+
+ordersRoute.post('/', validateToken, ordersController.set);
 
 export default ordersRoute;
