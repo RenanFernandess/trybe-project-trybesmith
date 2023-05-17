cd679fcaa2b4310ae71a0b894420b84fe6a7059b
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 14:39:12 -0300

    :saprkles: faet: Crai a função login na usersController

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,16 +2,23 @@ import { Request, Response } from 'express';
 import usersService from '../services/users.service';
 import TStatus from '../types/status';
 
-const set = async (req: Request, res: Response): Promise<void> => {
+const set = async (req: Request, res: Response) => {
   const { body: { username, vocation, level, password } } = req;
 
   const { status, message }: TStatus = await usersService
     .set({ username, vocation, level, password });
-  if (status) { res.status(status).json({ message }); return; }
+  if (status) return res.status(status).json({ message });
   
   res.status(201).json({ token: message });
 };
 
+const login = async ({ body }: Request, res: Response) => {
+  const { status, message }: TStatus = await usersService.login(body);
+  if (status) return res.status(status).json({ message }); 
+  res.status(200).json({ token: message });
+};
+
 export default {
   set,
+  login,
 };
