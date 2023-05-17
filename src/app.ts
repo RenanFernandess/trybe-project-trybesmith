05f91875e31234f3924058790c2a2eee7471ac4c
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 14:42:59 -0300

    :recycle: refactor: Usa o routes que contem todas as rotas

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import ordersRoute from './routes/orders.route';
-import productsRoute from './routes/products.route';
-import usersRoute from './routes/users.route';
+import routes from './routes';
 
 const app: express.Application = express();
 
 app.use(express.json());
 
-app.use('/products', productsRoute);
-app.use('/users', usersRoute);
-app.use('/orders', ordersRoute);
+app.use('/', routes);
 
 export default app;
