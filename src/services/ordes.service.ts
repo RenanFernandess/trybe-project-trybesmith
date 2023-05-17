d5f02c491493dfaa7c67813e419f7546d4501ef9
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 19:45:27 -0300

    :sparkles: feat: Cria a função set

--- src/services/ordes.service.ts
+++ src/services/ordes.service.ts
@@ -8,6 +8,20 @@ const getAll = async (): Promise<TStatus> => {
   return { status: null, message: orders };
 };
 
+const set = async ({ userId, productsIds }: TOrder) => {
+  if (!productsIds) return { status: 400, message: '"productsIds" is required' };
+  if (!Array.isArray(productsIds)) {
+    return { status: 422, message: '"productsIds" must be an array' };
+  }
+  if (!productsIds.length) { 
+    return { status: 422, message: '"productsIds" must include only numbers' }; 
+  }
+
+  await ordersModels.set({ userId, productsIds });
+  return { status: null, message: { userId, productsIds } };
+};
+
 export default {
   getAll,
+  set,
 };
