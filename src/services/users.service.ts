d06f5df3fafc4560c11b8df31868f5a4b13187c4
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 14:38:33 -0300

    :sparkles: feat: Cria a função login na usersService

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -10,6 +10,18 @@ const set = async (user: TUserRequest): Promise<TStatus> => {
   return { status: null, message: token };
 };
 
+const login = async ({ username, password }: Partial<TUser>) => {
+  if (!username) return { status: 400, message: '"username" is required' };
+  if (!password) return { status: 400, message: '"password" is required' };
+
+  const [user]: TUser[] = await usersModel.find({ username, password });
+  if (!user) return { status: 401, message: 'Username or password invalid' };
+
+  const token: string = createToken(user);
+  return { status: null, message: token };
+};
+
 export default {
   set,
+  login,
 };
