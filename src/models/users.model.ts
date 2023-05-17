2be7703651c89b6085b713f941ae4663dce69ed2
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 14:37:34 -0300

    :sparkles: feat: Cria a função find na usersModel

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -5,8 +5,7 @@ import connection from './connection';
 const set = async ({ username, vocation, level, password }: TUserRequest): Promise<TUser[]> => {
   const INSERT_QUERY = `
     INSERT INTO Trybesmith.users (username, vocation, level, password)
-    VALUES (?, ?, ?, ?)
-  `;
+    VALUES (?, ?, ?, ?)`;
   const SELECT_QUERY = 'SELECT id, username, vocation, level FROM Trybesmith.users WHERE id =?';
   const [{ insertId }] = await connection
     .execute<ResultSetHeader>(INSERT_QUERY, [username, vocation, level, password]);
@@ -15,6 +14,13 @@ const set = async ({ username, vocation, level, password }: TUserRequest): Promi
   return result;
 };
 
+const find = async ({ username, password }: Partial<TUser>) => {
+  const FIND_QUERY = 'SELECT * FROM Trybesmith.users WHERE username =? and password =?';
+  const [result] = await connection.execute(FIND_QUERY, [username, password]);
+  return result as TUser[];
+};
+
 export default {
   set,
+  find,
 };
