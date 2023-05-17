2a0c3fbb6cb1ec54bbeaaf64cdb13402527b109b
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 19:46:09 -0300

    :sparkles: feat: Cria a função set na ordersController

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import ordesService from '../services/ordes.service';
 import TStatus from '../types/status';
+import TOrder from '../types/order';
 
 const getAll = async (_req: Request, res: Response): Promise<void> => {
   const { status, message }: TStatus = await ordesService.getAll();
@@ -9,6 +10,16 @@ const getAll = async (_req: Request, res: Response): Promise<void> => {
   res.status(200).json(message);
 };
 
+const set = async ({ body }: Request, res: Response) => {
+  const { user: { id }, productsIds } = body;
+  const { status, message }: TStatus = await ordesService
+    .set({ userId: id, productsIds } as TOrder);
+  if (status) return res.status(status).json({ message });
+
+  res.status(201).json(message);
+};
+
 export default {
   getAll,
+  set,
 };
