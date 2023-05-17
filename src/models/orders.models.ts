8a6f68b3cc10f76f3ce2dc84482d983523a8bcf0
Renan Fernandes <renzinestuods@gmail.com>
2023-05-17 19:44:15 -0300

    :sparkles: feat: Cria a função create e set na ordersModel

--- src/models/orders.models.ts
+++ src/models/orders.models.ts
@@ -1,6 +1,7 @@
-import { RowDataPacket } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
 import TOrder from '../types/order';
 import connection from './connection';
+import productsModel from './products.model';
 
 const getAll = async (): Promise<TOrder[]> => {
   const SELECT_ORDER = (
@@ -14,10 +15,23 @@ const getAll = async (): Promise<TOrder[]> => {
     GROUP BY orders.id`
   );
   const [orders] = await connection.execute<RowDataPacket[] & TOrder[]>(SELECT_ORDER);
-
   return orders;
+};
+
+const create = async (userId: number) => {
+  const SET_ORDER = 'INSERT INTO Trybesmith.orders (user_id) VALUE (?)';
+  const [{ insertId }] = await connection.execute<ResultSetHeader>(SET_ORDER, [userId]);
+  return insertId;
+};
+
+const set = async ({ userId, productsIds }: TOrder) => {
+  const orderId = await create(userId);
+  const result = await productsModel.update({ orderId, productsIds });
+  return result;
 };
 
 export default {
   getAll,
+  set,
+  create,
 };
